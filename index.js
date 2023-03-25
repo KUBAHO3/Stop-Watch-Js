bdedd1ca4ab787646c192a2fd05d3e077da5b610
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-25 22:05:45 +0530

    select needed elements from DOM

---
+++ index.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  
+    var seconds = 00; 
+    var tens = 00; 
+    var appendTens = document.getElementById("tens")
+    var appendSeconds = document.getElementById("seconds")
+    var buttonStart = document.getElementById('button-start');
+    var buttonStop = document.getElementById('button-stop');
+    var buttonReset = document.getElementById('button-reset');
+    var Interval ;
+  
+   
+    
+  
+  }
