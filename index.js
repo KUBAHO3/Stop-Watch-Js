f23b885589da4c2499c2b5790ae10d1326f0faa8
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-25 22:08:33 +0530

    add start functionality

--- index.js
+++ index.js
@@ -9,7 +9,40 @@ window.onload = function () {
     var buttonReset = document.getElementById('button-reset');
     var Interval ;
   
-   
+    buttonStart.onclick = function() {
+      
+      clearInterval(Interval);
+       Interval = setInterval(startTimer, 10);
+    }
+    
+
+     
+    
+    function startTimer () {
+      tens++; 
+      
+      if(tens <= 9){
+        appendTens.innerHTML = "0" + tens;
+      }
+      
+      if (tens > 9){
+        appendTens.innerHTML = tens;
+        
+      } 
+      
+      if (tens > 99) {
+        console.log("seconds");
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+      }
+      
+      if (seconds > 9){
+        appendSeconds.innerHTML = seconds;
+      }
+    
+    }
     
   
   }
